333e99baf029b609234af76b2881e0a45a91b640
Paul Lisker <plisker@gmail.com>
2026-04-18 21:48:26 -0500

    Skip non-anchor nav links in scrolly init to unbreak mobile nav

--- story/assets/js/main.js
+++ story/assets/js/main.js
@@ -60,7 +60,9 @@
 			var $nav_a = $nav.find('a');
 
 			$nav_a
+				.filter(function() { return $(this).attr('href').charAt(0) === '#'; })
 				.addClass('scrolly')
+				.end()
 				.on('click', function() {
 
 					var $this = $(this);
@@ -81,8 +83,13 @@
 				.each(function() {
 
 					var	$this = $(this),
-						id = $this.attr('href'),
-						$section = $(id);
+						id = $this.attr('href');
+
+					// External link? Bail.
+						if (!id || id.charAt(0) !== '#')
+							return;
+
+					var	$section = $(id);
 
 					// No section for this link? Bail.
 						if ($section.length < 1)
